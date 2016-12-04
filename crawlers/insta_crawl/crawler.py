d0494fb7a00e42c785e6d243ce1ee6888dab0f39
Sagar Joglekar <sagarjoglekar@gmail.com>
2016-12-04 18:43:17 +0000

    Working crawler with user Cache

--- crawlers/insta_crawl/crawler.py
+++ crawlers/insta_crawl/crawler.py
@@ -3,6 +3,8 @@ import json
 import random
 import os, time
 import codecs, gzip, math
+import sys
+import cPickle as pickle
 
 
 class Utils:
@@ -17,20 +19,41 @@ class Utils:
             with gzip.open(fullpath  +'.gz', 'wb') as fout:
                 for line in fin:
                     fout.write(unicode(line).encode('utf-8'))
-        
+
         if delete_original:
             os.remove(fullpath)
-    @classmethod 
-    def download_video(cls, url, f_name):
+    @classmethod
+    def download_resource(cls, url, f_name):
         r = requests.get(url, stream = True)
+        print "Saving to file %s" %f_name
         with open(f_name, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=1024): 
+            for chunk in r.iter_content(chunk_size=1024):
                 if chunk: # filter out keep-alive new chunks
                     f.write(chunk)
         return f_name
 
 
 class User:
+    userDownloadCache = "InstaUsersCache.pk"
+    userRecords = os.getcwd() + "/" + userDownloadCache
+    print "Saving userRecords at %s" %userRecords
+
+    def getUserList(self):
+        visited = []
+        try:
+            f = open(self.userRecords, 'rb')
+            visited = pickle.load(f)
+            f.close()
+        except (EOFError, IOError) as e:
+            f = open(self.userRecords,"a+")
+            pickle.dump([],f)
+            f.close()
+        return visited
+
+    def updateUserList(self, listOfUsers):
+        with open(self.userRecords, 'wb') as f:
+            pickle.dump(listOfUsers, f)
+
     def search_user(self, u_name, uf_count='default'):
         url = "https://www.instagram.com/web/search/topsearch/"
         params = {}
@@ -125,7 +148,7 @@ class User:
                               }" % ( user_id, media_after, count),
                           "ref":"users::show",
                           "query_id": query_id
-                    } 
+                    }
         query_id = str(int(query_id) + 10)
         open('query_id.txt', 'w').write(query_id)
 
@@ -159,7 +182,7 @@ class User:
 class Post:
     def get_post( self, post_id, username, csrf_token, sessionid, mid ):
         url = 'https://www.instagram.com/p/%s/' % post_id
-        params = { 
+        params = {
             'taken-by' : username,
             '__a' : 1
         }
@@ -182,85 +205,112 @@ class Post:
 
         return requests.get(url, headers = q_header, params = params)
 
+def crawlUser(uname, destfolder , uSearchCount = 20 , postcount = 100, vidDownloadFlag = True , chunkSize=12):
+
+    ## input params ##
+    u_name = uname
+    dest_folder = destfolder + "/" +  u_name
+    uf_count = uSearchCount #'default' ; no of users searched from the query
+    post_count = postcount #'default'; 0 to pull posts; 10% to pull recent 10% of videos
+    vid_dwld_enabled = vidDownloadFlag
+    d_c = chunkSize # pagination; max = 128
+
+    if type(post_count) == str and '%' in post_count:
+        thresh_posts = 12 #min videos to fetch
+
+    ##################
 
 
-## input params ## 
-u_name = 'jackie'
-dest_folder = './insta_crawl_data/' + u_name
-uf_count = 10 #'default' ; no of users searched from the query
-post_count = 100 #'default'; 0 to pull posts; 10% to pull recent 10% of videos
-vid_dwld_enabled = True 
-d_c = 12 # pagination; max = 128
+    u = User()
+    user_search_resp = u.search_user(u_name, uf_count=uf_count)
+    csrf_token = user_search_resp.cookies['csrftoken']
+    sessionid = user_search_resp.cookies['sessionid']
+    searched_users = u.parse_and_save(user_search_resp.text, dest_folder)
+    if searched_users:
+        for user_data in searched_users:
 
-if type(post_count) == str and '%' in post_count:
-    thresh_posts = 12 #min videos to fetch
+            u_folder = u.store_user_data( dest_folder, user_data )
+            print "Storing is User folder : %s" %u_folder
+            u_data = user_data['user']
+            if not u_data['is_private']:
 
-##################
+                username =  u_data['username']
+                print "Crawling %s as a part of Username %s searched" %(username , u_name)
+                user_init_resp = u.get_user_init_data(username, csrf_token, sessionid )
 
-
-u = User() 
-user_search_resp = u.search_user(u_name, uf_count=uf_count)
-searched_users = u.parse_and_save(user_search_resp.text, dest_folder)
-if searched_users:
-    for user_data in searched_users:
-        u_folder = u.store_user_data( dest_folder, user_data )
-        u_data = user_data['user']
-        if not u_data['is_private']:
-            csrf_token = user_search_resp.cookies['csrftoken']
-            sessionid = user_search_resp.cookies['sessionid']
-            username =  u_data['username']
-            user_init_resp = u.get_user_init_data(username, csrf_token, sessionid )
-
-            user_init_data = json.loads(user_init_resp.text)
-
-            csrf_token = user_init_resp.cookies['csrftoken']
-            sessionid = user_init_resp.cookies['sessionid']
-            mid = user_init_resp.cookies['mid']
-            if not post_count == 'default':
-                if type(post_count) == str and '%' in post_count:
-                    post_count = int(post_count.split('%')[0].strip())
-                    if post_count < thresh_posts:
-                        post_count = thresh_posts
-
-                media_data = user_init_data['user']['media']
-                
-                if post_count > int(media_data['count']) or post_count==0:
-                    post_count = media_data['count']
-
-                post_counts = [d_c] * int(math.ceil(post_count*1.0/d_c))
-
+                user_init_data = json.loads(user_init_resp.text)
+                crawledUsers = u.getUserList()
                 user_id = user_init_data['user']['id']
-                media_after = media_data['page_info']['end_cursor']
-                has_next = media_data['page_info']['has_next_page']
-                for pg_cnt, post_count in enumerate(post_counts):
-                    if has_next:
-                        user_addl_data = u.get_user_full_data(username,user_id, media_after, post_count, csrf_token, sessionid, mid) #self, user_id, media_after, count, csrf_token, sessionid
-                        user_addl_data_j = json.loads(user_addl_data.text)
-                        addl_med_data = user_addl_data_j['media']
-                        user_init_data['user']['media']['nodes'].extend( addl_med_data['nodes'] )
-                        media_after = addl_med_data['page_info']['end_cursor']
-                        has_next = addl_med_data['page_info']['has_next_page']
-                        user_init_data['user']['media']['page_info' + '_' + str(pg_cnt+1)] = addl_med_data['page_info']
-                        time.sleep(0.1)
-            user_full_data = user_init_data
-            u.store_user_full_data(u_folder, user_full_data)
+                if user_id in crawledUsers:
+                    print "The User has been crawled before"
+                    break
+                else:
+                    crawledUsers.append(user_id)
+                    u.updateUserList(crawledUsers)
+
+                csrf_token = user_init_resp.cookies.get('csrftoken', csrf_token)
+
+                sessionid = user_init_resp.cookies.get('sessionid', sessionid)
+                mid = user_init_resp.cookies['mid']
+                if not post_count == 'default':
+                    media_data = user_init_data['user']['media']
+                    if type(post_count) == str and '%' in post_count:
+                        post_count = int(post_count.split('%')[0].strip())
+                        post_count = math.ceil(int(media_data['count'])   * (post_count) /100.0)
+                        if post_count < thresh_posts:
+                            post_count = thresh_posts
 
 
 
-            ########crawl each node#####################
-            post = Post()
-            for node in user_full_data['user']['media']['nodes']:
-                pst_dat = post.get_post( node['code'], username, csrf_token, sessionid, mid  )
-                pst_json = json.loads(pst_dat.text)
-                p_folder = u_folder + '/' + node['code']
-                Utils.check_create(p_folder)
-                p_file = p_folder +'/' + 'post_full_data.json'
-                with open(p_file, 'w') as outfile:
-                    json.dump(pst_json, outfile)
-                Utils.compress_utf8_file(p_file)
-                if vid_dwld_enabled:
-                   
-                    if pst_json['media']['is_video']:
-                        vid_link = pst_json['media']['video_url']
-                        v_file = p_folder +'/' + vid_link.split('/')[-1]
-                        Utils.download_video (vid_link, v_file )
+                    if post_count > int(media_data['count']) or post_count==0:
+                        post_count = media_data['count']
+
+                    post_counts = [d_c] * int(math.ceil(post_count*1.0/d_c))
+                    media_after = media_data['page_info']['end_cursor']
+                    has_next = media_data['page_info']['has_next_page']
+                    for pg_cnt, post_count in enumerate(post_counts):
+                        if has_next:
+                            user_addl_data = u.get_user_full_data(username,user_id, media_after, post_count, csrf_token, sessionid, mid) #self, user_id, media_after, count, csrf_token, sessionid
+                            user_addl_data_j = json.loads(user_addl_data.text)
+                            addl_med_data = user_addl_data_j['media']
+                            user_init_data['user']['media']['nodes'].extend( addl_med_data['nodes'] )
+                            media_after = addl_med_data['page_info']['end_cursor']
+                            has_next = addl_med_data['page_info']['has_next_page']
+                            user_init_data['user']['media']['page_info' + '_' + str(pg_cnt+1)] = addl_med_data['page_info']
+                            time.sleep(0.1)
+                user_full_data = user_init_data
+                u.store_user_full_data(u_folder, user_full_data)
+                ########crawl each node#####################
+                post = Post()
+                for node in user_full_data['user']['media']['nodes']:
+                    pst_dat = post.get_post( node['code'], username, csrf_token, sessionid, mid  )
+                    pst_json = json.loads(pst_dat.text)
+                    p_folder = u_folder + '/' + node['code']
+                    Utils.check_create(p_folder)
+                    p_file = p_folder +'/' + 'post_full_data.json'
+                    with open(p_file, 'w') as outfile:
+                        json.dump(pst_json, outfile)
+                    Utils.compress_utf8_file(p_file)
+                    if vid_dwld_enabled:
+
+
+                        if pst_json['media']['is_video']:
+                            link = pst_json['media']['video_url']
+                            v_file = p_folder +'/' + link.split('/')[-1]
+                            Utils.download_resource(link, v_file )
+                        else:
+                            link = pst_json['media']['display_src']
+                            v_file = p_folder +'/' + link.split('/')[-1].split('?')[0]
+                            Utils.download_resource (link, v_file )
+
+
+if __name__ == '__main__':
+    userFile = sys.argv[1]
+    destFolder = sys.argv[2]
+    if os.path.exists(userFile) and os.path.exists(destFolder):
+        f = open(userFile, 'rb' )
+        names = f.readlines()
+        for name in names:
+            crawlUser(name.strip() , destFolder  )
+
+
